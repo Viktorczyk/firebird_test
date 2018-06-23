4bc67976b6398ef3c6cde7d89562dc65e22eded5
wgniadkowski <viktorczyk@gmail.com>
2018-06-23 11:01:38 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.firebirdsql.jdbc.FBDriver");
+            connection  = DriverManager.getConnection(
+                    "jdbc:firebirdsql://localhost/N:\\Bazy\\Firebird2.5\\firebirdtest.FDB?serverTimezone=CET",
+                    "admin",
+                    "admin");
+
+
+            PreparedStatement preparedStatment =
+                    connection.prepareStatement(
+                            "Select * from m_kimwsp where nazwa like ?");
+            preparedStatment.setString(1, "%Fiat%");
+            ResultSet result = preparedStatment.executeQuery();
+            while (result.next()){
+                String marka = result.getString("Marka");
+                String model = result.getString("Model");
+                String year = result.getString("Rok");
+                Double price = result.getDouble("Cena");
+                System.out.println(marka+ " " +model+" "+year +" "+price);
+            }
+
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            connection.close();
+        }
+
+    }
+    }
+
